03dc4d137ba79ae559ed55dbfcac8d5c12340ffe
Jovy Cortez <jovycortez11@gmail.com>
2023-04-17 22:24:44 -0400

    Adding Main, Navbar components and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+import "./style.css"
+import { useState } from "react"
 
-function App() {
+export default function App() {
+  const [darkMode, setDarkMode] = useState(true)
+  
+  function toggleDarkMode() {
+      setDarkMode(prevMode => !prevMode)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
-  );
+      <div className="container">
+          <Navbar 
+              darkMode={darkMode} 
+              toggleDarkMode={toggleDarkMode}
+          />
+          <Main darkMode={darkMode} />
+      </div>
+  )
 }
-
-export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import logo from "../images/logo-darkmode.png"
+
+export default function Navbar(props) {
+
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src={ logo }
+                alt="logo"
+            />
+            <h3 className="nav--logo_text">Nil Hill</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
